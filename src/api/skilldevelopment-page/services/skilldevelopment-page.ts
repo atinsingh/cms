6e73dc1e3df41b416dc52b6ec9b66aac8cf7a0a5
abhinavroy121 <abhinavroy121.ar@gmail.com>
2023-08-30 14:09:11 +0530

    skill development, tutorial,accelerated

---
+++ src/api/skilldevelopment-page/services/skilldevelopment-page.ts
@@ -0,0 +1,7 @@
+/**
+ * skilldevelopment-page service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::skilldevelopment-page.skilldevelopment-page');
---
+++ src/api/tutorial-page/controllers/tutorial-page.ts
@@ -0,0 +1,7 @@
+/**
+ * tutorial-page controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::tutorial-page.tutorial-page');
