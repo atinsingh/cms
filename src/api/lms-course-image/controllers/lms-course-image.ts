e673f34bfac99934e66f688aa7843c5663f4d7d9
nicor528 <nicolasriquelme528@gmail.com>
2024-03-12 10:23:28 -0300

    3-12-24

---
+++ src/api/lms-course-image/controllers/lms-course-image.ts
@@ -0,0 +1,7 @@
+/**
+ * lms-course-image controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::lms-course-image.lms-course-image');
---
+++ src/api/lms-course-image/routes/lms-course-image.ts
@@ -0,0 +1,7 @@
+/**
+ * lms-course-image router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::lms-course-image.lms-course-image');
