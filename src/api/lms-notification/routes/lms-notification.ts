0d8f4664b9f9ffee55b9bfbbcbc024d52cb5a6aa
nicor528 <nicolasriquelme528@gmail.com>
2024-03-19 09:41:36 -0300

    3-19-24

---
+++ src/api/lms-notification/routes/lms-notification.ts
@@ -0,0 +1,7 @@
+/**
+ * lms-notification router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::lms-notification.lms-notification');
---
+++ src/api/lms-notification/services/lms-notification.ts
@@ -0,0 +1,7 @@
+/**
+ * lms-notification service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::lms-notification.lms-notification');
